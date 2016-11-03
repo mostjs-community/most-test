15c98a0dfd9ce93158bf62edd0dc5d47081ef24c
RamIdeas <rahul@ramideas.com>
2016-11-03 00:00:17 +0000

    Abstract environment from stream under test (#6)
    * Abstract environment from stream under test
    * Remove unnecessary most-subject dependency
    * Make "with reset()" tests match "without reset()"
    * Correct indentation and remove comments
    * Dispose of disposables on .reset()
    * Add .track() method to explicitly begin observing
    * Add .track() method to definitions file

--- type-definitions/most-test.d.ts
+++ type-definitions/most-test.d.ts
@@ -3,20 +3,55 @@ declare module "most-test" {
 
     import { Stream } from 'most'
 
-    export const run: <T>(stream: Stream<T>) => TestEnvironment<T>;
-
     type Time = Number;
     type Interval = Number;
+ 
+    export class TestEnvironment {
 
-    type TestEnvironment<T> = {
-        tick: (min?: Interval) => Promise<Result<T>>;
-        now: Time;
-        results: Result<T>[];
+        readonly now: Time;
+
+        constructor();
+
+        tick<T>( ms?: Interval ): this;
+        track( ...streams: Stream<any>[] ): this;
+        collect<T>( stream: Stream<T> ): Promise<Result<T>>;
+        results<T>( stream: Stream<T> ): Result<T>[];
+        reset(): void;
     }
 
     type Result<T> = {
-        events: Array<T>;
-        end?: true;
+        events: T[];
+        end?: true | { value: T };
         error?: Error;
+    }
+
+    export class BasicTestEnvironment<T> {
+
+        readonly now: Time;
+        readonly results: Result<T>[];
+
+        constructor( stream: Stream<T> );
+
+        tick<T>( ms?: Interval ): Promise<Result<T>>;
+    }
+
+    export function run<T>( stream: Stream<T> ): BasicTestEnvironment<T>;
+
+    class Sink<T> {
+        buckets: Bucket<T>[];
+        t: Time;
+        events: [string, Time, any][];
+        index: number;
+        next( t: Time ): Bucket<T>;
+        event( t: Time, x: T );
+        end( t: Time, x: T );
+        error( t: Time, err: Error );
+    }
+
+    class Bucket<T> {
+        events: T[];
+        end: true | { value: T };
+        error: Error;
+        toObject(): Result<T>;
     }
 }
